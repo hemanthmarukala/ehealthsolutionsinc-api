2a113ee2c19586705ef3299933f4e9b860c3553d
arvindcr2 <arvindcr2@gmail.com>
2017-01-11 14:52:59 -0800

    11th

--- cli-app/sqlcon.js
+++ cli-app/sqlcon.js
@@ -1,33 +1,32 @@
 var sql = require('mssql');
 
+sql.connect("mssql://sa:passw0rd!@localhost/Documents").then(function() {
+    // Query
 
+    new sql.Request().query('select * from Filetype').then(function(recordset) {
+        console.dir(recordset);
+    }).catch(function(err) {
+        // ... query error checks
+    });
 
-sql.connect("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=True;Initial Catalog=Documents;Data Source=Arvind;Use Procedure for Prepare=1;Auto Translate=True;Packet Size=4096;Workstation ID=ARVIND;Use Encryption for Data=False;Tag with column collation when possible=False
-").then(function() {
+    // Stored Procedure
 
-new sql.Request().query('select * from document').then(function(recordset){
-console.dir(recordset);
-}).catch(function(err){});
+    new sql.Request()
+    .input('input_parameter', sql.Int, value)
+    .output('output_parameter', sql.VarChar(50))
+    .execute('procedure_name').then(function(recordsets) {
+        console.dir(recordsets);
+    }).catch(function(err) {
+        // ... execute error checks
+    });
 
-new sql.Request().input('input_parameter', sql.Int, value)
-.output('output_parameter', sql.VarChar(50))
-.execute('procedure_name').then(function(recordsets){
-console.dir(recordsets);
-}).catch(function(err){
+    // ES6 Tagged template literals (experimental)
+
+    sql.query`select * from filetype`.then(function(recordset) {
+        console.dir(recordset);
+    }).catch(function(err) {
+        // ... query error checks
+    });
+}).catch(function(err) {
+    // ... connect error checks
 });
-
-sql.query`select * from document where id = $(value)`.then(function(recordset){
-console.dir(recordset);
-}).catch(function(err){
-
-});
-catch(function(err) {
-});
-
-//app.get('/', function(req, res)){
-//res.send('GET request to the homepage')
-//})
-
-//app.post('/', function(req, res)){
-//res.send('POST request to the homepage')
-//})
